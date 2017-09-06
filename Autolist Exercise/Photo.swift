f5284803a2ee9ea9380987d9d72c8b27ccf601fb
Richard Tackett <richtackett@hotmail.com>
2017-09-06 18:40:02 -0500

    Adding heart

--- Autolist Exercise/Photo.swift
+++ Autolist Exercise/Photo.swift
@@ -19,6 +19,15 @@ struct Photo {
     let server: String
     let secret: String
     let title: String
+    var isSelected = false
+    
+    init(id: String, farm: Int, server: String, secret: String, title: String) {
+        self.id = id
+        self.farm = farm
+        self.server = server
+        self.secret = secret
+        self.title = title
+    }
     
     var imageURL: URL? {
         return URL(string: "https://farm\(farm).staticflickr.com/\(server)/\(id)_\(secret).jpg")
--- Autolist Exercise/PhotoCollectionViewCell.swift
+++ Autolist Exercise/PhotoCollectionViewCell.swift
@@ -11,12 +11,12 @@ import Kingfisher
 
 class PhotoCollectionViewCell: UICollectionViewCell {
     fileprivate let imageView = UIImageView(frame: .zero)
+    fileprivate let favoriteButton = UIButton(frame: .zero)
+    fileprivate var photo: Photo?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        contentView.backgroundColor = UIColor.green
-        
+        contentView.backgroundColor = UIColor.white
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
@@ -27,6 +27,19 @@ class PhotoCollectionViewCell: UICollectionViewCell {
         NSLayoutConstraint(item: imageView, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1.0, constant: 10).isActive = true
         NSLayoutConstraint(item: contentView, attribute: .bottom, relatedBy: .equal, toItem: imageView, attribute: .bottom, multiplier: 1.0, constant: 10).isActive = true
         NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: contentView, attribute: .top, multiplier: 1.0, constant: 10).isActive = true
+        
+        favoriteButton.setImage(UIImage(named: "heart"), for: .normal)
+        favoriteButton.backgroundColor = UIColor.white.withAlphaComponent(0.60)
+        favoriteButton.layer.cornerRadius = 4.0
+        favoriteButton.clipsToBounds = true
+        favoriteButton.translatesAutoresizingMaskIntoConstraints = false
+        favoriteButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        
+        contentView.addSubview(favoriteButton)
+        NSLayoutConstraint(item: contentView, attribute: .trailing, relatedBy: .equal, toItem: favoriteButton, attribute: .trailing, multiplier: 1.0, constant: 20).isActive = true
+        NSLayoutConstraint(item: favoriteButton, attribute: .top, relatedBy: .equal, toItem: contentView, attribute: .top, multiplier: 1.0, constant: 20).isActive = true
+        NSLayoutConstraint(item: favoriteButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 40).isActive = true
+        NSLayoutConstraint(item: favoriteButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 40).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -39,6 +52,26 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     }
     
     func populate(_ photo: Photo) {
+        self.photo = photo
         imageView.kf.setImage(with: photo.imageURL)
+    }
+    
+    func buttonTapped(_ sender : UIButton) {
+        guard var photo = photo else {
+            return
+        }
+        
+        photo.isSelected = !photo.isSelected
+        self.photo = photo
+        
+        if photo.isSelected {
+            contentView.backgroundColor = UIColor.red
+            favoriteButton.setImage(UIImage(named: "solidHeart"), for: .normal)
+        } else {
+            contentView.backgroundColor = UIColor.white
+            favoriteButton.setImage(UIImage(named: "heart"), for: .normal)
+        }
+        
+        //call repo here to store in cache
     }
 }
